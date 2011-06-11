5f6bbdcd57546c168190412c8132b68511224e74
Mike Szczys <szczys@hotmail.com>
2011-06-11 14:56:32 -0500

    Improved comments

--- dst.c
+++ dst.c
@@ -6,11 +6,14 @@ char myMonth = 04;
 char myDOW = 0;
 char myNthWeek = 2;
 
-/***********************
-* Day of Week algorithm:
-* Sakamoto's Algorithm
-* http://en.wikipedia.org/wiki/Calculating_the_day_of_the_week#Sakamoto.27s_algorithm
-***********************/
+/*--------------------------------------------------------------------------
+  FUNC: 6/11/11 - Returns day of week for any given date
+  PARAMS: year, month, date
+  RETURNS: day of week (0-7 is Sun-Sat)
+  NOTES: Sakamoto's Algorithm
+    http://en.wikipedia.org/wiki/Calculating_the_day_of_the_week#Sakamoto.27s_algorithm
+    Altered to use char when possible to save microcontroller ram
+--------------------------------------------------------------------------*/
 char dow(int y, char m, char d)
    {
        static int t[] = {0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4};
@@ -18,6 +21,13 @@ char dow(int y, char m, char d)
        return (y + y/4 - y/100 + y/400 + t[m-1] + d) % 7;
    }
 
+/*--------------------------------------------------------------------------
+  FUNC: 6/11/11 - Returns the date for Nth day of month. For instance,
+    it will return the numeric date for the 2nd Sunday of April
+  PARAMS: year, month, day of week, Nth occurence of that day in that month
+  RETURNS: date
+  NOTES: There is no error checking for invalid inputs.
+--------------------------------------------------------------------------*/
 char NthDate(int year, char month, char DOW, char NthWeek){
   char targetDate = 1;
   char firstDOW = dow(year,month,targetDate);
@@ -31,6 +41,7 @@ char NthDate(int year, char month, char DOW, char NthWeek){
 }
 
 int main(void){
+  //Used to test on a computer
   sprintf(buff,"%i",NthDate(myYear,myMonth,myDOW,myNthWeek));
   printf("%s\n",buff);
 }
